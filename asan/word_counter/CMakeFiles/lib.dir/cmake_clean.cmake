317877f55b49d85f2ce01b5e53af489147789a56
Gmasem <gmasem@yandex.ru>
2020-03-06 14:44:30 +0500

    add allocator

---
+++ asan/word_counter/CMakeFiles/lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib.dir/reader.cpp.o"
+  "CMakeFiles/lib.dir/parser.cpp.o"
+  "liblib.pdb"
+  "liblib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
